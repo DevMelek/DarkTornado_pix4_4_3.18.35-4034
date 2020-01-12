556155a6f706f6a0dce33a449e5339cc45fc6c8d
DevMelek <56092115+DevMelek@users.noreply.github.com>
2020-01-12 18:13:24 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export KBUILD_BUILD_USER=skyrimus
-export KBUILD_BUILD_HOST=4pda
-export CROSS_COMPILE=/root/arm-eabi-4.8/bin/arm-eabi-
+export KBUILD_BUILD_USER=DEVMelek
+export KBUILD_BUILD_HOST=Melek
+export ARCH=arm CROSS_COMPILE=${PWD}/arm-eabi-4.8/bin/arm-eabi-
 export USE_CCACHE=1
 export ARCH=arm ARCH_MTK_PLATFORM=mt6580
 export TARGET=out
